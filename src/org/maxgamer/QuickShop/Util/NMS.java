0f3ec3487cb884df318bfd27c4d7b4bb30f08e22
Agaricus <agaricusb@yahoo.com>
2013-02-09 11:20:26 -0800

    Remove support for older NMS versions, all except v1_4_R1

--- src/org/maxgamer/QuickShop/Util/NMS.java
+++ src/org/maxgamer/QuickShop/Util/NMS.java
@@ -8,171 +8,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class NMS{
-	private static ArrayList<NMSDependent> dependents = new ArrayList<NMSDependent>();
-	
-	static{
-		NMSDependent dep;
-		
-		/* ***********************
-		 * **       1.4       ** *
-		 * ***********************/
-		dep = new NMSDependent(""){ //**NO EXCEPTION THROWN HERE**
-			@Override
-			public void safeGuard(Item item) {
-				ItemStack iStack = item.getItemStack();
-				
-				//Fetch the NMS item
-				net.minecraft.server.ItemStack nmsI = org.bukkit.craftbukkit.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Force the count to 0, don't notify anything though.
-				nmsI.count = 0;
-				//Get the itemstack back as a bukkit stack
-				iStack = org.bukkit.craftbukkit.inventory.CraftItemStack.asBukkitCopy(nmsI);
-				
-				//Set the display item to the stack.
-				item.setItemStack(iStack);
-			}
-			
-			@Override
-			public byte[] getNBTBytes(ItemStack iStack) {
-				net.minecraft.server.ItemStack is = org.bukkit.craftbukkit.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Save the NMS itemstack to a new NBT tag
-				net.minecraft.server.NBTTagCompound itemCompound = new net.minecraft.server.NBTTagCompound();
-				itemCompound = is.save(itemCompound);
-				
-				//Convert the NBT tag to a byte[]
-				return net.minecraft.server.NBTCompressedStreamTools.a(itemCompound);
-			}
-			
-			@Override
-			public ItemStack getItemStack(byte[] bytes) {
-				net.minecraft.server.NBTTagCompound c = net.minecraft.server.NBTCompressedStreamTools.a(bytes);
-				net.minecraft.server.ItemStack is = net.minecraft.server.ItemStack.createStack(c);
-				return org.bukkit.craftbukkit.inventory.CraftItemStack.asBukkitCopy(is);
-			}
-		};
-		dependents.add(dep);
-		
-		/* ***********************
-		 * **      1.4.5      ** *
-		 * ***********************/
-		dep = new NMSDependent("v1_4_5"){
-			@Override
-			public void safeGuard(Item item) {
-				ItemStack iStack = item.getItemStack();
-				
-				//Fetch the NMS item
-				net.minecraft.server.v1_4_5.ItemStack nmsI = org.bukkit.craftbukkit.v1_4_5.inventory.CraftItemStack.createNMSItemStack(iStack);
-				//Force the count to 0, don't notify anything though.
-				nmsI.count = 0;
-				//Get the itemstack back as a bukkit stack
-				iStack = org.bukkit.craftbukkit.v1_4_5.inventory.CraftItemStack.asBukkitStack(nmsI);
-				
-				//Set the display item to the stack.
-				item.setItemStack(iStack);
-			}
-			
-			@Override
-			public byte[] getNBTBytes(ItemStack iStack) {
-				net.minecraft.server.v1_4_5.ItemStack is = org.bukkit.craftbukkit.v1_4_5.inventory.CraftItemStack.createNMSItemStack(iStack);
-				//Save the NMS itemstack to a new NBT tag
-				net.minecraft.server.v1_4_5.NBTTagCompound itemCompound = new net.minecraft.server.v1_4_5.NBTTagCompound();
-				itemCompound = is.save(itemCompound);
-				
-				//Convert the NBT tag to a byte[]
-				return net.minecraft.server.v1_4_5.NBTCompressedStreamTools.a(itemCompound);
-			}
-			
-			@Override
-			public ItemStack getItemStack(byte[] bytes) {
-				net.minecraft.server.v1_4_5.NBTTagCompound c = net.minecraft.server.v1_4_5.NBTCompressedStreamTools.a(bytes);
-				net.minecraft.server.v1_4_5.ItemStack is = net.minecraft.server.v1_4_5.ItemStack.a(c);
-				return org.bukkit.craftbukkit.v1_4_5.inventory.CraftItemStack.asBukkitStack(is);
-			}
-		};
-		dependents.add(dep);
-		
-		/* ***********************
-		 * **      1.4.6      ** *
-		 * ***********************/
-		dep = new NMSDependent("v1_4_6"){
-			@Override
-			public void safeGuard(Item item) {
-				ItemStack iStack = item.getItemStack();
-				
-				//Fetch the NMS item
-				net.minecraft.server.v1_4_6.ItemStack nmsI = org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Force the count to 0, don't notify anything though.
-				nmsI.count = 0;
-				//Get the itemstack back as a bukkit stack
-				iStack = org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack.asBukkitCopy(nmsI);
-				
-				//Set the display item to the stack.
-				item.setItemStack(iStack);
-			}
-			
-			@Override
-			public byte[] getNBTBytes(ItemStack iStack) {
-				net.minecraft.server.v1_4_6.ItemStack is = org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Save the NMS itemstack to a new NBT tag
-				net.minecraft.server.v1_4_6.NBTTagCompound itemCompound = new net.minecraft.server.v1_4_6.NBTTagCompound();
-				itemCompound = is.save(itemCompound);
-				
-				//Convert the NBT tag to a byte[]
-				return net.minecraft.server.v1_4_6.NBTCompressedStreamTools.a(itemCompound);
-			}
-			
-			@Override
-			public ItemStack getItemStack(byte[] bytes) {
-				net.minecraft.server.v1_4_6.NBTTagCompound c = net.minecraft.server.v1_4_6.NBTCompressedStreamTools.a(bytes);
-				net.minecraft.server.v1_4_6.ItemStack is = net.minecraft.server.v1_4_6.ItemStack.a(c);
-				return org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack.asBukkitCopy(is);
-			}
-		};
-		dependents.add(dep);
-		
-		/* ***********************
-		 * **      1.4.7      ** *
-		 * ***********************/
-		dep = new NMSDependent("v1_4_R1"){
-			@Override
-			public void safeGuard(Item item) {
-				ItemStack iStack = item.getItemStack();
-				
-				//Fetch the NMS item
-				net.minecraft.server.v1_4_R1.ItemStack nmsI = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Force the count to 0, don't notify anything though.
-				nmsI.count = 0;
-				//Get the itemstack back as a bukkit stack
-				iStack = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asBukkitCopy(nmsI);
-				
-				//Set the display item to the stack.
-				item.setItemStack(iStack);
-			}
-			
-			@Override
-			public byte[] getNBTBytes(ItemStack iStack) {
-				net.minecraft.server.v1_4_R1.ItemStack is = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asNMSCopy(iStack);
-				//Save the NMS itemstack to a new NBT tag
-				net.minecraft.server.v1_4_R1.NBTTagCompound itemCompound = new net.minecraft.server.v1_4_R1.NBTTagCompound();
-				itemCompound = is.save(itemCompound);
-				
-				//Convert the NBT tag to a byte[]
-				return net.minecraft.server.v1_4_R1.NBTCompressedStreamTools.a(itemCompound);
-			}
-			
-			@Override
-			public ItemStack getItemStack(byte[] bytes) {
-				net.minecraft.server.v1_4_R1.NBTTagCompound c = net.minecraft.server.v1_4_R1.NBTCompressedStreamTools.a(bytes);
-				net.minecraft.server.v1_4_R1.ItemStack is = net.minecraft.server.v1_4_R1.ItemStack.createStack(c);
-				return org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asBukkitCopy(is);
-			}
-		};
-		dependents.add(dep);
-	}
-	
-	/** The known working NMSDependent. This will be null if we haven't found one yet. */
-	private static NMSDependent nms;
-	
 	/**
 	 * Sets the given items stack size to 0,
 	 * as well as gives it a custom NBT tag
@@ -184,8 +19,17 @@ public class NMS{
 	 */
 	public static void safeGuard(Item item) throws ClassNotFoundException{
 		rename(item.getItemStack());
-		validate();
-		nms.safeGuard(item);
+		ItemStack iStack = item.getItemStack();
+
+		//Fetch the NMS item
+		net.minecraft.server.v1_4_R1.ItemStack nmsI = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asNMSCopy(iStack);
+		//Force the count to 0, don't notify anything though.
+		nmsI.count = 0;
+		//Get the itemstack back as a bukkit stack
+		iStack = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asBukkitCopy(nmsI);
+
+		//Set the display item to the stack.
+		item.setItemStack(iStack);
 	}
 	
 	/** 
@@ -208,8 +52,13 @@ public class NMS{
 	 * @throws ClassNotFoundException if QS is not updated to this build of bukkit.
 	 */
 	public static byte[] getNBTBytes(ItemStack iStack) throws ClassNotFoundException{
-		validate();
-		return nms.getNBTBytes(iStack);
+		net.minecraft.server.v1_4_R1.ItemStack is = org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asNMSCopy(iStack);
+		//Save the NMS itemstack to a new NBT tag
+		net.minecraft.server.v1_4_R1.NBTTagCompound itemCompound = new net.minecraft.server.v1_4_R1.NBTTagCompound();
+		itemCompound = is.save(itemCompound);
+
+		//Convert the NBT tag to a byte[]
+		return net.minecraft.server.v1_4_R1.NBTCompressedStreamTools.a(itemCompound);
 	}
 	
 	/**
@@ -219,43 +68,8 @@ public class NMS{
 	 * @throws ClassNotFoundException if QS is not updated to this build of bukkit.
 	 */
 	public static ItemStack getItemStack(byte[] bytes) throws ClassNotFoundException{
-		validate();
-		return nms.getItemStack(bytes);
-	}
-	
-	/**
-	 * Finds the proper NMS version.  If a version is already
-	 * selected, this method does nothing, even if the version
-	 * is invalid.
-	 * @throws ClassNotFoundException If there is no found working version.
-	 */
-	private static void validate() throws ClassNotFoundException{
-		if(nms != null) return;
-		for(NMSDependent dep : dependents){
-			if(dep.isValid() == false) continue;
-			nms = dep;
-			return;
-		}
-		throw new ClassNotFoundException("This version of QuickShop is incompatible."); //We haven't got code to support your version!
-	}
-	
-	private static abstract class NMSDependent{
-		private String version;
-		public NMSDependent(String version){ this.version = version; }
-		public abstract void safeGuard(Item item);
-		public abstract byte[] getNBTBytes(ItemStack iStack);
-		public abstract ItemStack getItemStack(byte[] bytes);
-		/** Returns true if this can be used as a NMS version */
-		public boolean isValid(){
-			try{
-				//Debug
-				String clazz = "net.minecraft.server"+(version == null || version.isEmpty() ? "" : "."+version)+".ItemStack";
-				System.out.println("Testing: " + clazz);
-				Class.forName(clazz);
-				System.out.println("Valid.");
-				return true; 
-			}
-			catch(ClassNotFoundException e){ return false; }
-		}
+		net.minecraft.server.v1_4_R1.NBTTagCompound c = net.minecraft.server.v1_4_R1.NBTCompressedStreamTools.a(bytes);
+		net.minecraft.server.v1_4_R1.ItemStack is = net.minecraft.server.v1_4_R1.ItemStack.createStack(c);
+		return org.bukkit.craftbukkit.v1_4_R1.inventory.CraftItemStack.asBukkitCopy(is);
 	}
 }
